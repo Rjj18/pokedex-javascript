c5c061358771377b57fade54190340d057ece4ca
Rjj18 <roger.psn12@gmail.com>
2023-11-25 08:21:22 -0300

    add: load button and generation limit

--- assets/js/pokeapi.js
+++ assets/js/pokeapi.js
@@ -1,11 +1,13 @@
-const pokeapi = {}
+const pokeApi = {}
 
-function convertPokeApiDetailToPokemon (pokeDetail) {
+function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
+
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
+
     pokemon.types = types
     pokemon.type = type
 
@@ -14,19 +16,19 @@ function convertPokeApiDetailToPokemon (pokeDetail) {
     return pokemon
 }
 
-pokeapi.getPokemonDetail = (pokemon) => {
+pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeapi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
-    .then ((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map(pokeapi.getPokemonDetail))
-    .then((detailRequests) => Promise.all(detailRequests))
-    .then((pokemonsDetails) => pokemonsDetails)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
